ea91c071e4ebb6005b438fe45f6b3b99ea6fec32
Sam <130783534+Sam-programs@users.noreply.github.com>
2023-05-10 03:45:36 +0300

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,8 +4,8 @@ function set_theme(theme,airline_theme)
    airline_theme = airline_theme or 'base16_dracula';
 	vim.cmd.colorscheme(theme)
    vim.cmd.AirlineTheme(airline_theme)
-   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+   --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+   --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
